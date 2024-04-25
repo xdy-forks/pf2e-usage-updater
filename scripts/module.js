28e503c39f2866da8ca548339371981ae6de699c
Chas <79132112+ChasarooniZ@users.noreply.github.com>
2024-04-25 01:01:06 -0400

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -3,5 +3,7 @@ Hooks.once('init', async function() {
 });
 
 Hooks.once('ready', async function() {
-
+  Hooks.on("updateWorldTime", async (_total, diff) => {
+    //DO stuff to the usage counts on actors
+  })
 });
